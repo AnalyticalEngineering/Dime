fde8fc0b429af9bb14aa9b848de3f3c6ffe3648b
Joseph DeWeese <jdeweese@weldtascics.systems>
2023-12-21 12:37:57 -0600

    Initial Commit

---
+++ Dime/DimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DimeApp.swift
+//  Dime
+//
+//  Created by J. DeWeese on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct DimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
