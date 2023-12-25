90cf7e327e23f55a05f44e2749ea887f03a2404a
Joseph DeWeese <jdeweese@weldtascics.systems>
2023-12-25 16:35:47 -0600

    improve

---
+++ Dime/MODELS/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  Dime
+//
+//  Created by J. DeWeese on 12/22/23.
+//
+
+import SwiftUI
+
+enum Category: String, CaseIterable  {
+    case income = "Income"
+    case expense = "Expense"
+}
---
+++ Dime/VIEWS/Views/Tab Views/Graphs.swift
@@ -0,0 +1,18 @@
+//
+//  Graphs.swift
+//  Dime
+//
+//  Created by J. DeWeese on 12/22/23.
+//
+
+import SwiftUI
+
+struct Graphs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Graphs()
+}
